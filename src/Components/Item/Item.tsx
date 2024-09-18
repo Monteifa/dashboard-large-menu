52710e8bb193477458f7852130a3c77fc252520d
Monteifa <fabiopmont@gmail.com>
2024-09-18 19:36:51 -0300

    Fix layout css

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -9,10 +9,17 @@ interface ItemProps {
   data: Array<Data>;
   index?: number;
   style: object;
+  selectedItem: Data | undefined;
   setSelectedItem: (item: Data) => void;
 }
 
-const Item = ({ data, index, style, setSelectedItem }: ItemProps) => {
+const Item = ({
+  data,
+  index,
+  style,
+  selectedItem,
+  setSelectedItem,
+}: ItemProps) => {
   if (index === undefined || index === null || !data) {
     return null;
   }
@@ -20,8 +27,11 @@ const Item = ({ data, index, style, setSelectedItem }: ItemProps) => {
   const item = data[index];
 
   return (
-    <div style={style} className='item_container'>
-      <span className=''>{item?.name}</span>
+    <div
+      style={style}
+      className={`item_container ${selectedItem?.id === item.id && 'selected'}`}
+    >
+      <span>{item?.name}</span>
 
       <span>{format(item?.price)}</span>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,6 +58,7 @@ const Home = () => {
                   data={data}
                   index={index}
                   style={style}
+                  selectedItem={selectedItem}
                   setSelectedItem={setSelectedItem}
                 />
               )}
